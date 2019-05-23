c4bd512c9c2f82820957032c77ec94015987bf65
aditj <aditjain1980@gmail.com>
2019-05-23 18:29:12 +0530

    Basic Changes

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/adit/5d04464b-934c-455c-af4f-f0e6a43d237d/gitty/django-back/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
