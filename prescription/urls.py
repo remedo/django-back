a2f66e100ff5e26c8e2ed1c7ac87f937a4ab3867
aditj <aditjain1980@gmail.com>
2019-05-29 15:39:45 +0530

    added basic template and views for displaying a prescription by prescription id

--- prescription/urls.py
+++ prescription/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 urlpatterns = [
 	path('',views.index,name='index'),
+	path('<prescription_id>/', views.pres_view, name='view'),
 ]
-
--- prescription/views.py
+++ prescription/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Prescription
+from django.shortcuts import render
+#There's a high positive chance of this not working.
 # Create your views here.
 def index(request):
     return HttpResponse("Prescription")
+#method to get a particular prescription
+def pres_view(request,prescription_id):
+    curr_pres=Prescription.objects.filter(prescription_id=prescription_id)
+    context= {'pres':curr_pres}
+    return render(request, 'prescription/view.html', context)
